2a06a5a7948b4ee087e79394c4c43753601531b9
ADRITA CHAKRABORTY <chakrabortyadrita04@gmail.com>
2024-06-29 03:46:16 +0530

    sagnik bhai decoder

--- index.mjs
+++ index.mjs
@@ -2,12 +2,22 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import connectDB from './config/database.mjs';
 import userRoutes from './routes/usersRoutes.mjs';
+import cors from 'cors';
+
 
 
 const app = express();
 connectDB();
 
 app.use(bodyParser.json());
+app.use (express.json());
+app.use (
+                        cors(
+                                                {
+                                                                        origin:"*",
+                                                }
+                        )
+)
 
 app.use('/api/users', userRoutes);
 app.get('/', (req, res) => {
