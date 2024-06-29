92a60e6e515f091c0e7cf59313c20f615dda9f28
ADRITA CHAKRABORTY <chakrabortyadrita04@gmail.com>
2024-06-29 18:17:27 +0530

    getme api added

--- controller/usersController.mjs
+++ controller/usersController.mjs
@@ -75,7 +75,7 @@ const login = async (req, res) => {
     }
 };
 
-const userById = async (req, res) => {
+/*const userById = async (req, res) => {
     try {
       const userId = req.params.userId;
   
@@ -96,5 +96,20 @@ const userById = async (req, res) => {
       console.error('Error fetching user by ID:', error);
       res.status(500).json({ message: 'Server error' });
     }
-  }
-export default { register, login ,userById};
+  }*/
+    const getMe = async (req, res) => {
+        try {
+          const userId = req.userId;
+      
+          const user = await User.findById(userId);
+          if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+          }
+      
+          res.status(200).json(user);
+        } catch (error) {
+          console.error('Error fetching user data:', error);
+          res.status(500).json({ message: 'Server error' });
+        }
+      };
+export default { register, login , getMe};
--- routes/usersRoutes.mjs
+++ routes/usersRoutes.mjs
@@ -1,9 +1,11 @@
 import express from 'express';
 const router = express.Router();
 import userController from '../controller/usersController.mjs';
+import auth from '../middlewares/auth.mjs'
 
 router.post('/register', userController.register);
 router.post('/login', userController.login);
-router.get('/userId/:userId',userController.userById);
+//router.get('/userId/:userId',userController.userById);
+router.get('/me', auth, userController.getMe);
 
 export default router;
